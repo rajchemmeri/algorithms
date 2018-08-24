cf82931d1a09282859141217784a1c891849241d
rajchemmeri <rajeshchemmery@gmail.com>
2018-08-24 14:05:00 -0700

    Update SpiralMatrix.java

--- src/com/rajchemmeri/algorithms/SpiralMatrix.java
+++ src/com/rajchemmeri/algorithms/SpiralMatrix.java
@@ -7,10 +7,10 @@ public class SpiralMatrix {
 	public static void main(String[] args) {
 		
 		char[][] matrix = new char[][] { {  'a', 'b', 'c', 'd', 'e' },
-									                   {  'f', 'g', 'h', 'i', 'j' },
-									                   {  'j', 'k', 'l', 'm', 'n' },
-									                   {  'o', 'p', 'q', 'r', 's' },
-									                 };
+						 {  'f', 'g', 'h', 'i', 'j' },
+						 {  'j', 'k', 'l', 'm', 'n' },
+						 {  'o', 'p', 'q', 'r', 's' },
+					       };
 		int last_row = 	 matrix.length ;
 		int last_col =   matrix[0].length;
 		
@@ -18,25 +18,25 @@ public class SpiralMatrix {
     
 		while(i< last_row && j < last_col) {
 			
-              for(int k =0;k<last_col;++k) {
-                System.out.print(matrix[i][k]);
-              }
-              ++i;
+			      for(int k =0;k<last_col;++k) {
+				System.out.print(matrix[i][k]);
+			      }
+			      ++i;
 
-              for(int l=i;l<last_row;++l) {
-                System.out.print(matrix[l][last_col-1]);
-              }
-              --last_col;
+			      for(int l=i;l<last_row;++l) {
+				System.out.print(matrix[l][last_col-1]);
+			      }
+			      --last_col;
 
-              for(int k= last_col-1;k>=j;--k) {
-                System.out.print(matrix[last_row-1][k]);
-              }
-              --last_row;
+			      for(int k= last_col-1;k>=j;--k) {
+				System.out.print(matrix[last_row-1][k]);
+			      }
+			      --last_row;
 
-              for(int k= last_row-1;k>=i;--k) {
-                System.out.print(matrix[k][j]);
-              }
-              ++j;
+			      for(int k= last_row-1;k>=i;--k) {
+				System.out.print(matrix[k][j]);
+			      }
+			      ++j;
 		}
 		
 		
