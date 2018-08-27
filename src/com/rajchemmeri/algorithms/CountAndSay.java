453c03f4f698d86d3333cb2705f5f9dbeeb256e8
rajchemmeri <rajeshchemmery@gmail.com>
2018-08-26 22:46:28 -0700

    Update CountAndSay.java

--- src/com/rajchemmeri/algorithms/CountAndSay.java
+++ src/com/rajchemmeri/algorithms/CountAndSay.java
@@ -3,7 +3,7 @@ package array;
 public class CountAndSay {
 
 	public static void main(String[] args) {
-		LSay obj = new LSay();
+		CountAndSay obj = new CountAndSay();
 		obj.countAndSay(6);
 	}
 
@@ -14,31 +14,31 @@ public class CountAndSay {
 		String result = "";
 
 		for(int i = 0;i< n-1; ++i ) {
-        int cno = 0;
-        int pno = 0;
-        int j = 0;
-        int count = 0;
-        StringBuilder builder =  new StringBuilder();	
-                while( j <= input.length()) {
+			int cno = 0;
+			int pno = 0;
+			int j = 0;
+			int count = 0;
+			StringBuilder builder =  new StringBuilder();	
+			while( j <= input.length()) {
 
-                  if( j < input.length()) {
-                      cno = input.charAt(j) - 48;
-                          if( j == 0 ) {
-                              count+=1;
-                              pno = cno;
-                          }else if( pno == cno) {
-                              count+=1;
-                          }else {
-                              builder.append(count).append(pno);
-                              pno = cno;
-                              count =1;
-                          }
-                  }else {
-                    result = builder.append(count).append(pno).toString();
-                  }
-				   ++j;
-			  }
-			     input = result;
+				  if( j < input.length()) {
+				      cno = input.charAt(j) - 48;
+					  if( j == 0 ) {
+					      count+=1;
+					      pno = cno;
+					  }else if( pno == cno) {
+					      count+=1;
+					  }else {
+					      builder.append(count).append(pno);
+					      pno = cno;
+					      count =1;
+					  }
+				  }else {
+					result = builder.append(count).append(pno).toString();
+				  }
+			      ++j;
+			}
+		   input = result;
 
 		}
 		  System.out.println(input);
